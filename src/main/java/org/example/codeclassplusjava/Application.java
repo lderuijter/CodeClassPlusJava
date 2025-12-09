197e065536ce1176da77555ab7cc9bbd649c631a
lderuijter <lucasder2003@gmail.com>
2025-12-09 13:35:41 +0100

    added some content to the Application VBox

--- src/main/java/org/example/codeclassplusjava/Application.java
+++ src/main/java/org/example/codeclassplusjava/Application.java
@@ -2,7 +2,7 @@ package org.example.codeclassplusjava;
 
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,23 +11,21 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         VBox root = new VBox();
-        Scene scene = new Scene(root, 800, 600);
-
-        Button button = new Button("Hello World!");
-        TextField textField = new TextField("default text");
-        Label melding = new Label();
-
-        button.setOnAction(event -> {
-            String textFieldContent = textField.getText();
-            melding.setText("Hallo " + textFieldContent);
-        });
-
-        root.getChildren().add(textField);
-        root.getChildren().add(button);
-        root.getChildren().add(melding);
-
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(root, 900, 600);
         stage.setScene(scene);
+
+        String title = "F1CC";
+        stage.setTitle(title);
+
+        Button button = new Button("Click me!");
+        Label label = new Label("Hello World!");
+
+        root.getChildren().add(button);
+        root.getChildren().add(label);
+
+        button.setOnAction(e -> label.setText("You clicked me!"));
+
+        stage.setResizable(false);
         stage.show();
     }
 }
