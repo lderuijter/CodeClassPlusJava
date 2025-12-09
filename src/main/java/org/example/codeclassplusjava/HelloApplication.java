8e57297171f95b8f60826d87a14ef65a42904110
lderuijter <lucasder2003@gmail.com>
2025-12-09 11:23:54 +0100

    changes to remove FXML

--- src/main/java/org/example/codeclassplusjava/HelloApplication.java
+++ src/main/java/org/example/codeclassplusjava/HelloApplication.java
@@ -1,8 +1,9 @@
 package org.example.codeclassplusjava;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,22 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FlowPane root = new FlowPane();
+        Scene scene = new Scene(root, 800, 600);
+
+        Button button = new Button("Hello World!");
+        TextField textField = new TextField("default text");
+        Label melding = new Label();
+
+        button.setOnAction(event -> {
+            String textFieldContent = textField.getText();
+            melding.setText("Hallo " + textFieldContent);
+        });
+
+        root.getChildren().add(textField);
+        root.getChildren().add(button);
+        root.getChildren().add(melding);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
