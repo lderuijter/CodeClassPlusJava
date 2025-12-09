44d70db02966db52a8213e04eb9a437d5b8e4d02
lderuijter <lucasder2003@gmail.com>
2025-12-09 14:00:52 +0100

    Pane as layout and positions changed

--- src/main/java/org/example/codeclassplusjava/Application.java
+++ src/main/java/org/example/codeclassplusjava/Application.java
@@ -1,19 +1,17 @@
 package org.example.codeclassplusjava;
 
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        VBox root = new VBox();
+        Pane root = new Pane();
         Scene scene = new Scene(root, 900, 600);
         stage.setScene(scene);
 
@@ -21,9 +19,15 @@ public class Application extends javafx.application.Application {
         stage.setTitle(title);
 
         Button button = new Button("Click here!");
-        Label label = new Label(title);
+        button.setPrefSize(100, 25);
+        button.setLayoutX(400);
+        button.setLayoutY(300);
 
-       root.getChildren().addAll(label, button);
+        Label label = new Label(title);
+        label.setLayoutX(435);
+        label.setLayoutY(350);
+
+       root.getChildren().addAll(button, label);
 
         AtomicInteger counter = new AtomicInteger();
 
